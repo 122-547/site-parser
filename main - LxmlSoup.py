3bd4f08f9492d466df1c5f607d81f134bd0645d9
122-547 <122397402+122-547@users.noreply.github.com>
2023-12-04 18:53:11 +0300

    close

--- main - LxmlSoup.py
+++ main - LxmlSoup.py
@@ -17,26 +17,79 @@
 
 #code's end
 
+# import requests
+# from LxmlSoup import LxmlSoup
+
+# def fail_function(function_text_name):
+#        return f"We can't find this word: {function_text_name}"
+
+
+# try:
+#        url = "https://psycatgames.com/ru/magazine/conversation-starters/things-to-talk-about/"
+#        text_name = input("Search Word: ")
+       
+       
+       
+#        html = requests.get(url).text
+#        soup = LxmlSoup(html)
+       
+#        link = soup.find_all('h3')
+#        for i in link:
+#               tag_name = i.text
+#               print(f"Find Text: {tag_name}")
+
+# except:
+#        fail_function(text_name)
+
+#code
+
+
+# import requests
+# from LxmlSoup import LxmlSoup
+
+# def failed_progress(word_name):
+#        return f"We can't find {word_name}"
+
+# url = "https://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F"
+# word_name = input("Search Word: ")
+
+# html = requests.get(url).text
+# soup = LxmlSoup(html)
+# sentences = soup.find(text=word_name).text
+
+# print(sentences)
+       
+#code
+
 import requests
 from LxmlSoup import LxmlSoup
 
-def fail_function(function_text_name):
-       return f"We can't find this word: {function_text_name}"
+url = "https://ru.wikipedia.org/wiki/Википедия"
+html = requests.get(url).text
+soup = list(LxmlSoup(html).text())
+text = ""
+list_ = []
+words = []
+word = ""
+num = 0
+enter = 0
+por = 1
+for i in range(len(soup)):
+       i_t = i + 1
+       if i != len(soup) - 1:
+              if soup[i] + soup[i_t] != ". ":
+                     text = text + soup[i]
+              else:
+                     text = text + "."
+                     list_.append(text)
+                     text = ""
+       
 
-
-try:
-       url = "https://psycatgames.com/ru/magazine/conversation-starters/things-to-talk-about/"
-       text_name = input("Search Word: ")
-       
-       
-       
-       html = requests.get(url).text
-       soup = LxmlSoup(html)
-       
-       link = soup.find_all('h3')
-       for i in link:
-              tag_name = i.text
-              print(f"Find Text: {tag_name}")
-
-except:
-       fail_function(text_name)
+for g in list_:
+              if g.find(';') == -1:
+                     if g.find(':') == -1:
+                             if g.find('"') == -1:
+                                     if g.find("'") == -1:
+                                          
+                                          if g.find("Википедия") != -1 and len(g) >= 20:
+                                                 print(f"::{g}")
